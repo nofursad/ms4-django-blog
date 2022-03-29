a89eb5e3ea64a7502b5481124b384a51ae0f84f1
Abhisek Joshi <nofursad@gmail.com>
2022-03-29 20:43:16 +0000

    Deployment Commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import cloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(  )
 
 # Create your models here.
